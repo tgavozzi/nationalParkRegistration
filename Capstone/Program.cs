8402d88e624de6d79671e59746abbf26448ee4b1
Josh Tucholski <josh@techelevator.com>
2017-10-04 21:47:44 -0400

    Added initial files for capstone.

---
+++ Capstone/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
